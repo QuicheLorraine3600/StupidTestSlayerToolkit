860ad9263a5fe1c1338cb3e56e3d0bfd352373e1
Clément Moréna <clement.morena@epfl.ch>
2023-05-07 16:53:00 +0200

    + template_generator

--- template_generator.py
+++ template_generator.py
@@ -2,7 +2,7 @@
 number_of_lines = int(input("Number of lines of asm code: "))
 with open("1.csv", "w") as file1, open("2.csv", "w") as file2, open("3.csv", "w") as file3:
 	for i in range(number_of_lines):
-		line = i * "* ," + "\n"
+		line = i * "* , " + "\n"
 		file1.write(line)
 		file2.write(line)
 		file3.write(line)
